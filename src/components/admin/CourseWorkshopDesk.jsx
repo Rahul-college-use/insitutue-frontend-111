0428d188d1fbb8516568d2b91a9777efd45442c6
Rahul-college-use <rahulcollegeuse@gmail.com>
2026-07-03 20:43:20 +0530

    update

--- src/components/admin/CourseWorkshopDesk.jsx
+++ src/components/admin/CourseWorkshopDesk.jsx
@@ -43,8 +43,8 @@ export default function CourseWorkshopDesk({ selectedCourseId, coursesList, trig
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 5;
 
-  const url = "http://localhost:3000";
-  // const url = "https://internshipplace-bend.vercel.app";
+  // const url = "http://localhost:3000";
+  const url = "https://internshipplace-bend.vercel.app";
   const activeCourse = coursesList.find(c => c._id === selectedCourseId);
 
   // Stream Extraction Utilities
